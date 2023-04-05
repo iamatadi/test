be92bf849528668f174284f396303554674828d1
iamatadi <129717036+iamatadi@users.noreply.github.com>
2023-04-05 12:16:26 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace sample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
